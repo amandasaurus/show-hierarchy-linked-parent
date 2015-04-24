800a519fcb9161185a575f5b255fec7e9d9bf332
Rory McCann <rory@geofabrik.de>
2015-04-24 09:52:08 +0200

    fix module name

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version="0.1.0",
     author="Rory McCann",
     author_email="rory@technomancy.org",
-    py_modules=['showhierachy'],
+    py_modules=['showhierarchy'],
     platforms=['any',],
     requires=[],
     entry_points={
         'console_scripts': [
-            'showhierachy = showhierachy:main',
+            'showhierarchy = showhierarchy:main',
         ]
     },
     classifiers=[
